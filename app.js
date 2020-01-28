f8c9ed5eb689ce996508ccf78e0b0731d38cfdf5
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-01-27 18:40:51 -0600

    Create a sitewide configuration file

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// configuring the config.dev.js
+var config = require('./config.dev');
+//Test the file 
+// console.log(config);
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
