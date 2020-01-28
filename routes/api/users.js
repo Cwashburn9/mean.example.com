d493376bd7ea074649a68cb40c2743bf43afe8de
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-01-27 20:17:07 -0600

    GET/Read one

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var Users = require('../../models/users');
-
-router.get('/', function(req, res, next) {
-  Users.find({},function(err, users){
-    if(err){
-     return res.json({'success':false, 'error': err});
-   }
-    return res.json({'success':true, 'users': users});
-  });
+// GET/Read All
+router.get('/', function (req, res, next) {
+    Users.find({}, function (err, users) {
+        if (err) {
+            return res.json({ 'success': false, 'error': err });
+        }
+        return res.json({ 'success': true, 'users': users });
+    });
 });
+// Get/Read One
+router.get('/:userId', function (req, res) {
+
+    var userId = req.params.userId;
+    Users.findOne({ '_id': userId }, function (err, user) {
+        if (err) {
+            return res.json({ 'success': false, 'error': err });
+        }
+        return res.json({ 'success': true, 'user': user });
+    });
+});
+// Post/Create
+
+
 
 module.exports = router;
