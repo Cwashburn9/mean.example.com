65e20e2ae6531309b83d77dcca92973005474eef
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-03 21:45:53 -0600

    Add gulp processes for the user app

--- gulpfile.js
+++ gulpfile.js
@@ -7,46 +7,58 @@ var concat = require('gulp-concat');
 var merge = require('merge-stream');
 var scss = require('gulp-sass');
 
-gulp.task('build-main-css', function(){
+gulp.task('build-main-css', function () {
 
   var main = gulp.src([
     'src/scss/main.scss',
     'src/scss/forms.scss'
   ])
-  . pipe(scss())
-  . pipe(cleanCSS())
-  . pipe(concat('main.min.css'))
-  . pipe(gulp.dest('public/dist/css'));
+    .pipe(scss())
+    .pipe(cleanCSS())
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest('public/dist/css'));
 
   return merge(main);
 });
 
-gulp.task('build-main-js', function() {
+gulp.task('build-main-js', function () {
 
   var authApp = gulp.src([
     'src/js/main.js',
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('public/dist/js'));
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
 
   return merge(authApp);
 });
 
-gulp.task('build-auth-js', function() {
+gulp.task('build-auth-js', function () {
 
   var authApp = gulp.src([
     'src/js/auth.app.js',
   ])
-  .pipe(concat('auth.app.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('public/dist/js'));
+    .pipe(concat('auth.app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
 
   return merge(authApp);
 });
 
+gulp.task('build-users-js', function() {
+
+  var userApp = gulp.src([
+    'src/js/users.app.js',
+  ])
+  .pipe(concat('users.app.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/dist/js'));
+
+  return merge(userApp);
+});
+
 //Recompile SCSS/JS on save
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('./src/scss/**/*.scss', gulp.series('build-css'));
   gulp.watch('./src/js/**/*.js', gulp.series('build-js'));
 });
@@ -58,7 +70,8 @@ gulp.task('default', gulp.series('watch'));
 gulp.task('build-css', gulp.series('build-main-css'));
 
 //Compile all JS tasks
-gulp.task('build-js', gulp.series(
-    'build-main-js',
-    'build-auth-js',
-  ));
+gulp.task('build-js', [
+  'build-main-js',
+  'build-auth-js',
+  'build-users-js'
+]);
