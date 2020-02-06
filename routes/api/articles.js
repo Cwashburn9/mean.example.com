3985a2b7a84356789be880c56faa7ab8e4b681e1
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-06 01:30:01 -0600

    .../api/applications {sucess:true, articles[]}

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,111 +1,79 @@
 var express = require('express');
 var router = express.Router();
-var Articles = require('../../models/articles');
+var articles = require('../../models/articles');
 
-// GET/Read All
 router.get('/', function (req, res, next) {
-  Articles.find({}, function (err, articles) {
-      if (err) {
-          return res.json({ 'success': false, 'error': err });
-      }
-      return res.json({ 'success': true, 'articles': articles });
+  articles.find({}, function (err, articles) {
+    if (err) {
+      return res.json({ 'success': false, 'error': err });
+    }
+    return res.json({ 'success': true, 'articles': articles });
   });
 });
 
-// Get/Read One
-router.get('/:articleId', function (req, res) {
-
-  var articleId = req.params.articleId;
-  Articles.findOne({ '_id': articleId }, function (err, article) {
-      if (err) {
-          return res.json({ 'success': false, 'error': err });
-      }
-      return res.json({ 'success': true, 'article': article });
+router.get('/:id', function (req, res) {
+  var id = req.params.id;
+  articles.findOne({ '_id': id }, function (err, article) {
+    if (err) {
+      return res.json({ 'success': false, 'error': err });
+    }
+    return res.json({ 'success': true, 'article': article });
   });
 });
 
-// Post/Create
 router.post('/', function (req, res) {
-  Articles.create(new Articles({
-      title: req.body.title,
-      description: req.body.description,
-      keywords: req.body.keywords,
-      body: req.body.body,
-      published: req.body.published
+  articles.create(new articles({
+    title: req.body.title,
+    description: req.body.description,
+    keywords: req.body.keywords,
+    body: req.body.body,
+    published: req.body.published
   }), function (err, article) {
-
-      if (err) {
-          return res.json({ success: false, article: req.body, error: err });
-      }
-
-      return res.json({ success: true, article: article });
-
+    if (err) {
+      return res.json({ success: false, article: req.body, error: err });
+    }
+    return res.json({ success: true, article: article });
   });
 });
-
-// Post/Update
 router.put('/', function (req, res) {
-
-  Articles.findOne({ '_id': req.body._id }, function (err, article) {
-
-      if (err) {
+  articles.findOne({ '_id': req.body._id }, function (err, article) {
+    if (err) {
+      return res.json({ success: false, error: err });
+    } else if (article) {
+      let data = req.body;
+      if (data.title) {
+        article.title = data.title;
+      }
+      if (data.description) {
+        article.description = data.description;
+      }
+      if (data.keywords) {
+        article.keywords = data.keywords;
+      }
+      if (data.body) {
+        article.body = data.body;
+      }
+      if (data.published) {
+        article.published = data.published;
+        article.offset = new Date(data.published).getTimezoneOffset();
+      }
+      article.save(function (err) {
+        if (err) {
           return res.json({ success: false, error: err });
-      }
-
-      if (article) {
-
-          let data = req.body;
-
-          if (data.title) {
-              article.title = data.title;
-          };
-
-          if (data.description) {
-              article.description = data.description;
-          };
-
-          if (data.keywords) {
-              article.keywords = data.keywords;
-          };
-
-          if (data.body) {
-              article.body = data.body;
-          };
-
-          if (data.published) {
-            article.published = data.published;
-        };
-
-          article.save(function (err) {
-              if (err) {
-                  return res.json({ success: false, error: err });
-              } else {
-                  return res.json({ success: true, article: article });
-              }
-          });
-
-      }
-
+        } else {
+          return res.json({ success: true, article: article });
+        }
+      });
+    }
   });
-
 });
-
-// delete/delete
 router.delete('/:articleId', function (req, res) {
-
   var articleId = req.params.articleId;
-
-  Articles.remove({ '_id': articleId }, function (err, removed) {
-
-      if (err) {
-          return res.json({ success: false, error: err });
-      }
-
-      return res.json({ success: true, status: removed });
-
+  articles.remove({ '_id': articleId }, function (err, removed) {
+    if (err) {
+      return res.json({ success: false, error: err });
+    }
+    return res.json({ success: true, status: removed });
   });
-
 });
-
-
 module.exports = router;
